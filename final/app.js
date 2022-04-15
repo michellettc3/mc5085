d6e95e038cd5491117be5015016f598878c4c819
michellettc3 <mc5085@columbia.edu>
2022-04-15 17:27:26 -0400

    April15

--- final/app.js
+++
@@ -1,8 +0,0 @@
-const scrollable = document.querySelector('.scrollable');
-const content = document.querySelector('.content');
-const imgSections = [...document.querySelectorAll('.img-section')];
-const images= [...document.querySelectorAll('.img')];
-images.forEach((image, idx) => {
-    image.style.backgroundImage = 'url(./images/${idx + 1}.'
-})
-
