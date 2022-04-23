67792c4047786d28234bf64414640bbd14615c63
michellettc3 <mc5085@columbia.edu>
2022-04-23 00:11:57 -0400

    4/23

--- practice/:)/script.js
+++ practice/:)/script.js
@@ -99,7 +99,64 @@ let reverse = false;
 
 document.addEventListener('click', () => reverse = !reverse)
 
+class HoverButton {
+    constructor(id) {
+        this.hovered = false;
+        this.animatingHover = false;
+        this.forceOut = false;
+        this.timing = 0.65;
+        this.el = document.getElementById(id);
+        this.bg = this.el.getElementsByClassName("bg")[0];
+        this.el.addEventListener("mouseenter", this.onMouseEnter);
+        this.el.addEventListener("mouseleave", this.onMouseLeave);
+    }
 
+    onMouseEnter = () => {
+        this.hoverInAnim();
+    };
+
+    hoverInAnim = () => {
+        if (!this.hovered) {
+            this.hovered = true;
+            this.animatingHover = true;
+            this.forceOut = false;
+            TweenMax.fromTo(
+                this.bg,
+                this.timing,
+                {x: "-112%"},
+                {
+                    x: "-12%",
+                    ease: Power3.easeOut,
+                    onComplete: () => {
+                        this.animatingHover = false;
+                        if (this.forceOut) {
+                            this.foceOut = false;
+                            this.hoverOutAnim();
+                        }
+                    }
+                }
+            );
+        }
+    };
+
+    onMouseLeave = () => {
+        if (!this.animatingHover) {
+            this.hoverOutAnim();
+        } else {
+            this.forceOut = true;
+        }
+    };
+
+    hoverOutAnim = () => {
+        this.hovered = false;
+        TweenMax.to(this.bg, this.timing, {
+            x: "100%",
+            ease: Power3.easeOut,
+            onComplete: () => {
+            }
+        });
+    };
+}
 
 class Dot {
     constructor(index = 0) {
